eb53e0aac043d61a17db241eb4cb68c7a64c0fc5
DevI99 <roth.gamedev@gmail.com>
2026-07-24 12:13:06 +0200

    Fixing some other issues with the csproj file
    Also adds a test scene and un-comments the LookupScriptsInAssembly in MainFile

---
+++ CampfireTradeCode/Testing/NCustomSceneTest.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+
+namespace CampfireTrade.CampfireTradeCode.Testing;
+
+public partial class NCustomSceneTest : CanvasLayer
+{
+    public override void _Ready()
+    {
+        GD.Print("[CampfireTrade] Custom test scene initialized");
+        
+        Button closeButton = GetNode<Button>("Center/Panel/Content/CloseButton");
+
+        closeButton.Pressed += QueueFree;
+    }
+}
