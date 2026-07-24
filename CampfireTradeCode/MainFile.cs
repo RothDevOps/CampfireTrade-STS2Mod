6822a9221bb083a4c5206445afcfde2074365ad7
DevI99 <roth.gamedev@gmail.com>
2026-07-24 10:01:24 +0200

    Updates csproj file to include sts2.dll for Godot process
    Some other tweaks to the json and MainFile

--- CampfireTradeCode/MainFile.cs
+++ CampfireTradeCode/MainFile.cs
@@ -10,8 +10,7 @@ public partial class MainFile : Node
 {
     public const string ModId = "CampfireTrade"; //At the moment, this is used only for the Logger and harmony names.
 
-    public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } =
-        new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
+    public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } = new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
 
     public static void Initialize()
     {
